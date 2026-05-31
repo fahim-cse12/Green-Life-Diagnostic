8c40742ba9b72dfdb4a8edd4b51790ca79488ff6
Md. Fahim Muntasir <34747383+fahim-cse12@users.noreply.github.com>
2026-05-30 23:30:34 -0400

    Updated

--- Service.Contracts/IRoleService.cs
+++ Service.Contracts/IRoleService.cs
@@ -1,12 +1,5 @@
-﻿using Entities.Models;
-using Entities.Responses;
+﻿using Entities.Responses;
 using Microsoft.AspNetCore.Identity;
-using Shared.DataTransferObject;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Service.Contracts
 {
